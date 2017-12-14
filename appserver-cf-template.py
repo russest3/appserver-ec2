323b73bbb63fced19cecac052924b896918b5e2d
root <root@gringo.medfusion.local>
2017-12-13 20:37:38 -0500

    alksjdf

--- appserver-cf-template.py
+++ appserver-cf-template.py
@@ -45,15 +45,9 @@ from awacs.sts import AssumeRole
 ApplicationPort = "6000"
 PublicCidrIp = str(ip_network(get_ip()))
 
-ud = Base64(Join('\n', [
-    "#!/bin/bash",
-    "sudo yum -y update\n"
-    "sudo yum install --enablerepo=epel -y nodejs\n"
-]))
-
 t = Template()
 
-t.add_description("Effective DevOps in AWS: HelloWorld web application")
+t.add_description("Appserver Template")
 
 t.add_parameter(Parameter(
     "KeyPair",
@@ -96,25 +90,6 @@ t.add_parameter(Parameter(
     ConstraintDescription='must be a valid EC2 T2 instance type.',
 ))
 
-t.add_resource(Role(
-    "Role",
-    AssumeRolePolicyDocument=Policy(
-        Statement=[
-            Statement(
-                Effect=Allow,
-                Action=[AssumeRole],
-                Principal=Principal("Service", ["ec2.amazonaws.com"])
-            )
-        ]
-    )
-))
-
-t.add_resource(InstanceProfile(
-    "InstanceProfile",
-    Path="/",
-    Roles=[Ref("Role")]
-))
-
 t.add_resource(ec2.SecurityGroup(
     "SecurityGroup",
     GroupDescription="Allow SSH and TCP/{} access".format(ApplicationPort),
@@ -132,30 +107,7 @@ t.add_resource(ec2.SecurityGroup(
             CidrIp="0.0.0.0/0",
         ),
     ],
-))
-
-t.add_resource(IAMPolicy(
-    "Policy",
-    PolicyName="AllowS3",
-    PolicyDocument=Policy(
-        Statement=[
-            Statement(
-                Effect=Allow,
-                Action=[Action("s3", "*")],
-                Resource=["*"])
-        ]
-    ),
-    Roles=[Ref("Role")]
-))
-
-t.add_resource(LaunchConfiguration(
-    "LaunchConfiguration",
-    UserData=ud,
-    ImageId="ami-a4c7edb2",
-    KeyName=Ref("KeyPair"),
-    SecurityGroups=[Ref("SecurityGroup")],
-    InstanceType=Ref("InstanceType"),
-    IamInstanceProfile=Ref("InstanceProfile"),
+    VpcId=Ref("VpcId"),
 ))
 
 t.add_resource(ec2.SecurityGroup(
@@ -197,6 +149,77 @@ t.add_resource(elb.LoadBalancer(
     CrossZone=True,
     Subnets=Ref("PublicSubnet"),
     SecurityGroups=[Ref("LoadBalancerSecurityGroup")],
+))
+
+ud = Base64(Join('\n', [
+    "#!/bin/bash",
+    "sudo yum -y update\n"
+    "sudo yum install --enablerepo=epel -y nodejs\n"
+]))
+
+t.add_resource(Role(
+    "Role",
+    AssumeRolePolicyDocument=Policy(
+        Statement=[
+            Statement(
+                Effect=Allow,
+                Action=[AssumeRole],
+                Principal=Principal("Service", ["ec2.amazonaws.com"])
+            )
+        ]
+    )
+))
+
+t.add_resource(InstanceProfile(
+    "InstanceProfile",
+    Path="/",
+    Roles=[Ref("Role")]
+))
+
+t.add_resource(IAMPolicy(
+    "Policy",
+    PolicyName="AllowS3",
+    PolicyDocument=Policy(
+        Statement=[
+            Statement(
+                Effect=Allow,
+                Action=[Action("s3", "*")],
+                Resource=["*"])
+        ]
+    ),
+    Roles=[Ref("Role")]
+))
+
+t.add_resource(IAMPolicy(
+    "MonitoringPolicy",
+    PolicyName="AllowSendingDataForMonitoring",
+    PolicyDocument=Policy(
+        Statement=[
+            Statement(
+                Effect=Allow,
+                Action=[
+                    Action("cloudwatch", "Put*"),
+                    Action("logs", "Create*"),
+                    Action("logs", "Put*"),
+                    Action("logs", "Describe*"),
+                    Action("events", "Put*"),
+                    Action("firehose", "Put*"),
+                    Action("autoscaling", "DescribeAutoScalingInstances"),
+                ],
+                Resource=["*"])
+        ]
+    ),
+    Roles=[Ref("Role")]
+))
+
+t.add_resource(LaunchConfiguration(
+    "LaunchConfiguration",
+    UserData=ud,
+    ImageId="ami-a4c7edb2",
+    KeyName=Ref("KeyPair"),
+    SecurityGroups=[Ref("SecurityGroup")],
+    InstanceType=Ref("InstanceType"),
+    IamInstanceProfile=Ref("InstanceProfile"),
 ))
 
 t.add_resource(AutoScalingGroup(
@@ -260,277 +283,6 @@ t.add_resource(Alarm(
     ComparisonOperator="GreaterThanThreshold",
     AlarmActions=[Ref("ScaleUpPolicy"), ],
     InsufficientDataActions=[Ref("ScaleUpPolicy")],
-))
-
-t.add_resource(IAMPolicy(
-    "MonitoringPolicy",
-    PolicyName="AllowSendingDataForMonitoring",
-    PolicyDocument=Policy(
-        Statement=[
-            Statement(
-                Effect=Allow,
-                Action=[
-                    Action("cloudwatch", "Put*"),
-                    Action("logs", "Create*"),
-                    Action("logs", "Put*"),
-                    Action("logs", "Describe*"),
-                    Action("events", "Put*"),
-                    Action("firehose", "Put*"),
-                    Action("autoscaling", "DescribeAutoScalingInstances"),
-                ],
-                Resource=["*"])
-        ]
-    ),
-    Roles=[Ref("Role")]
-))
-
-print t.to_json()
-"""Generating CloudFormation template."""
-from ipaddress import ip_network
-
-from ipify import get_ip
-
-from troposphere import (
-    Base64,
-    ec2,
-    GetAtt,
-    Join,
-    Output,
-    Parameter,
-    Ref,
-    Template,
-    elasticloadbalancing as elb,
-)
-
-from troposphere.iam import (
-    InstanceProfile,
-    PolicyType as IAMPolicy,
-    Role,
-)
-
-from awacs.aws import (
-    Action,
-    Allow,
-    Policy,
-    Principal,
-    Statement,
-)
-
-from troposphere.autoscaling import (
-    AutoScalingGroup,
-    LaunchConfiguration,
-    ScalingPolicy,
-)
-
-from troposphere.cloudwatch import (
-    Alarm,
-    MetricDimension,
-)
-
-from awacs.sts import AssumeRole
-
-ApplicationPort = "6000"
-PublicCidrIp = str(ip_network(get_ip()))
-
-ud = Base64(Join('\n', [
-    "#!/bin/bash",
-    "sudo yum -y update\n"
-    "sudo yum install --enablerepo=epel -y nodejs\n"
-]))
-
-t = Template()
-
-t.add_description("Effective DevOps in AWS: HelloWorld web application")
-
-t.add_parameter(Parameter(
-    "KeyPair",
-    Description="Name of an existing EC2 KeyPair to SSH",
-    Type="AWS::EC2::KeyPair::KeyName",
-    ConstraintDescription="must be the name of an existing EC2 KeyPair.",
-))
-
-t.add_parameter(Parameter(
-    "VpcId",
-    Type="AWS::EC2::VPC::Id",
-    Description="VPC"
-))
-
-t.add_parameter(Parameter(
-    "PublicSubnet",
-    Description="PublicSubnet",
-    Type="List<AWS::EC2::Subnet::Id>",
-    ConstraintDescription="PublicSubnet"
-))
-
-t.add_parameter(Parameter(
-    "ScaleCapacity",
-    Default="2",
-    Type="String",
-    Description="Number servers to run",
-))
-
-t.add_parameter(Parameter(
-    'InstanceType',
-    Type='String',
-    Description='WebServer EC2 instance type',
-    Default='t2.micro',
-    AllowedValues=[
-        't2.micro',
-        't2.small',
-        't2.medium',
-        't2.large',
-    ],
-    ConstraintDescription='must be a valid EC2 T2 instance type.',
-))
-
-t.add_resource(ec2.SecurityGroup(
-    "SecurityGroup",
-    GroupDescription="Allow SSH and TCP/{} access".format(ApplicationPort),
-    SecurityGroupIngress=[
-        ec2.SecurityGroupRule(
-            IpProtocol="tcp",
-            FromPort="22",
-            ToPort="22",
-            CidrIp="0.0.0.0/0",
-        ),
-        ec2.SecurityGroupRule(
-            IpProtocol="tcp",
-            FromPort=ApplicationPort,
-            ToPort=ApplicationPort,
-            CidrIp="0.0.0.0/0",
-        ),
-    ],
-))
-
-t.add_resource(LaunchConfiguration(
-    "LaunchConfiguration",
-    UserData=ud,
-    ImageId="ami-a4c7edb2",
-    KeyName=Ref("KeyPair"),
-    SecurityGroups=[Ref("SecurityGroup")],
-    InstanceType=Ref("InstanceType"),
-    IamInstanceProfile=Ref("InstanceProfile"),
-))
-
-t.add_resource(AutoScalingGroup(
-    "AutoscalingGroup",
-    DesiredCapacity=Ref("ScaleCapacity"),
-    LaunchConfigurationName=Ref("LaunchConfiguration"),
-    MinSize=1,
-    MaxSize=2,
-    LoadBalancerNames=[Ref("LoadBalancer")],
-    VPCZoneIdentifier=Ref("PublicSubnet"),
-))
-
-t.add_resource(ec2.SecurityGroup(
-    "LoadBalancerSecurityGroup",
-    GroupDescription="Web load balancer security group.",
-    VpcId=Ref("VpcId"),
-    SecurityGroupIngress=[
-        ec2.SecurityGroupRule(
-            IpProtocol="tcp",
-            FromPort="3000",
-            ToPort="3000",
-            CidrIp="0.0.0.0/0",
-        ),
-    ],
-))
-
-t.add_resource(elb.LoadBalancer(
-    "LoadBalancer",
-    Scheme="internet-facing",
-    Listeners=[
-        elb.Listener(
-            LoadBalancerPort="3000",
-            InstancePort="3000",
-            Protocol="HTTP",
-            InstanceProtocol="HTTP"
-        ),
-    ],
-    HealthCheck=elb.HealthCheck(
-        Target="HTTP:3000/",
-        HealthyThreshold="5",
-        UnhealthyThreshold="2",
-        Interval="20",
-        Timeout="15",
-    ),
-    ConnectionDrainingPolicy=elb.ConnectionDrainingPolicy(
-        Enabled=True,
-        Timeout=10,
-    ),
-    CrossZone=True,
-    Subnets=Ref("PublicSubnet"),
-    SecurityGroups=[Ref("LoadBalancerSecurityGroup")],
-))
-
-t.add_resource(Role(
-    "Role",
-    AssumeRolePolicyDocument=Policy(
-        Statement=[
-            Statement(
-                Effect=Allow,
-                Action=[AssumeRole],
-                Principal=Principal("Service", ["ec2.amazonaws.com"])
-            )
-        ]
-    )
-))
-
-t.add_resource(InstanceProfile(
-    "InstanceProfile",
-    Path="/",
-    Roles=[Ref("Role")]
-))
-
-t.add_resource(IAMPolicy(
-    "Policy",
-    PolicyName="AllowS3",
-    PolicyDocument=Policy(
-        Statement=[
-            Statement(
-                Effect=Allow,
-                Action=[Action("s3", "*")],
-                Resource=["*"])
-        ]
-    ),
-    Roles=[Ref("Role")]
-))
-
-t.add_resource(IAMPolicy(
-    "MonitoringPolicy",
-    PolicyName="AllowSendingDataForMonitoring",
-    PolicyDocument=Policy(
-        Statement=[
-            Statement(
-                Effect=Allow,
-                Action=[
-                    Action("cloudwatch", "Put*"),
-                    Action("logs", "Create*"),
-                    Action("logs", "Put*"),
-                    Action("logs", "Describe*"),
-                    Action("events", "Put*"),
-                    Action("firehose", "Put*"),
-                    Action("autoscaling", "DescribeAutoScalingInstances"),
-                ],
-                Resource=["*"])
-        ]
-    ),
-    Roles=[Ref("Role")]
-))
-
-t.add_output(Output(
-    "InstancePublicIp",
-    Description="Public IP of our instance.",
-    Value=GetAtt("instance", "PublicIp"),
-))
-
-t.add_output(Output(
-    "WebUrl",
-    Description="Application endpoint",
-    Value=Join("", [
-        "http://", GetAtt("instance", "PublicDnsName"),
-        ":", ApplicationPort
-    ]),
 ))
 
 print t.to_json()
